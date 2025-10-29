40300b5b55cb62cac2ff305e8482fe8bb8d3bf05
Siva010q <suryaarts110@gmail.com>
2025-10-29 13:10:16 +0530

    Some Changes 😋

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const colorDivs = document.querySelectorAll(".list2 div");
-    alert("Press Any Key to Change the colors!");
-    function getRandomColor() {
-        const letter = "0123456789ABCDEF"; 
-        let color = "#"
-        for (let i = 0; i < 6; i++){
-            color += letter[Math.floor(Math.random() * 16 )];
-        }
+  const colorDivs = document.querySelectorAll(".list2 div");
+  const colorBtn = document.getElementById("colorBtn");
 
-        return color;
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+  }
 
-    function changeColors() {
-        colorDivs.forEach(div => {
-            div.style.backgroundColor = getRandomColor();
-        });
-    }
-        
-
-    document.addEventListener("keydown", (event) => {
-        event.preventDefault();
-        changeColors();
+  function changeColors() {
+    colorDivs.forEach(div => {
+      div.style.backgroundColor = getRandomColor();
     });
+  }
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+      changeColors();
+    }
+  });
+
+  colorBtn.addEventListener("click", changeColors);
 });
